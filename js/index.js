f5d29df987ef4833ca5b37bc318a80eda089229b
claire phillips <claireacam@gmail.com>
2020-12-28 23:08:54 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -3,8 +3,8 @@ var height = "74"
 if (name === "Susan") {
     console.log("The name is correct")
 }
-else (name===""Joe"){
-console.log ("Expectred the name to be Susan")
+else{
+console.log ("Expectred the name to be Susan")}
 // Don't worry about this
 module.exports = { name, height
 }
